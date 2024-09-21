f509ac95fc76c5ea92b8aec66cb8583ca30914b1
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-09-22 00:55:32 +0530

    react router added

--- frontend/collage-ui/src/App.js
+++ frontend/collage-ui/src/App.js
@@ -1,12 +1,13 @@
-
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-     Application
-    </div>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
   );
 }
 
